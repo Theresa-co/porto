d1dca0fcf1da4bb2200df55072b6a356d8a08f59
Theresa-co <satrioadi401@gmail.com>
2023-10-13 15:00:44 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="./assets/css/style.css">
+
+  <title>Adi Satrio Wibowo</title>
+</head>
+
+<body>
+  <div class="wrapper">
+    <section class="mainSection">
+      <div class="container-full-bg">
+        <div class="row">
+          <div class="jumbotron d-flex align-items-center">
+            <div class="container text-center">
+              <h1 class="" data-aos="fade-down">Hello,</h1>
+              <p class="" data-aos="fade-down">Adi Satrio Wibowo, S.Kom</p>
+              <div class="line m-auto"></div>
+              <p class="mt-3" data-aos="fade-up">"Jika kamu tidak sanggup menahan lelahnya belajar maka kamu harus sanggup menahan perihnya kebodohan" - Imam Syafi'i</p>
+              <a href="#portofolio" class="btn btn-primary btn-lg" data-aos="fade-up" role="button">START</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="container">
+
+        <div class="text-center" id="portofolio">
+          <h2 class="" id="portofolio">Portofolio</h2>
+          <div class="line m-auto"></div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-4 mt-4">
+            <div class="card" data-aos="fade-up">
+              <img src="./assets/images/terbang.PNG" class="card-img-top" alt="images">
+              <div class="card-body">
+                <h4 class="card-title">Terang Bangsa Christian School</h4>
+                <p class="card-text">Company Profile Sekolah Terang Bangsa, dibangun menggunakan Codeigniter 3. Bootstrap 4 & Database MySql</p>
+              </div>
+              <div class="col">
+                <a class="button fa fa-external-link" href="#">Visit</a>
+                <a class="button fa fa-youtube-play" href="#">Youtube</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4 mt-4">
+            <div class="card" data-aos="fade-up">
+              <img src="./assets/images/ppdb_terbang.PNG" class="card-img-top" alt="images">
+              <div class="card-body">
+                <h4 class="card-title">PPDB Terang Bangsa Christian School</h4>
+                <p class="card-text">Mengembangkan sistem PPDB yang sudah ada, dengan menambahkan fitur send ke email setiap jenjang yang didaftar, menggunakan PHPMailer</p>
+              </div>
+              <div class="col">
+                <a class="button fa fa-external-link" href="#">Visit</a>
+                <a class="button fa fa-youtube-play" href="#">Youtube</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4 mt-4">
+            <div class="card" data-aos="fade-up">
+              <img src="./assets/images/baliho.PNG" class="card-img-top" alt="images">
+              <div class="card-body">
+                <h4 class="card-title">SIM Baliho Diskominfo Kota Semarang</h4>
+                <p class="card-text">Sistem Informasi Manajemen Baliho Diskominfo Kota Semarang, dibangun menggunakan PHP Native, Leaflet & Database MySql</p>
+              </div>
+              <div class="col">
+                <a class="button fa fa-external-link" href="#">Visit</a>
+                <a class="button fa fa-youtube-play" href="#">Youtube</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-4 mt-4">
+            <div class="card" data-aos="fade-up">
+              <img src="./assets/images/inventory.PNG" class="card-img-top" alt="images">
+              <div class="card-body">
+                <h4 class="card-title">Inventory Assets QRCode & QRCode Reader Flutter Webiew</h4>
+                <p class="card-text">Aplikasi manajemen assets kantor dengan QRCode generator & Flutter webview sebagai QRcode reader</p>
+              </div>
+              <div class="col">
+                <a class="button fa fa-external-link" href="#">Visit</a>
+                <a class="button fa fa-youtube-play" href="#">Youtube</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4 mt-4">
+            <div class="card" data-aos="fade-up">
+              <img src="./assets/images/inke.PNG" class="card-img-top" alt="images">
+              <div class="card-body">
+                <h4 class="card-title">POS Inke Production</h4>
+                <p class="card-text">Aplikasi untuk mengelola toko roti Inke Bakery, mulai dari request order sampai ke produksi dan stok bahan baku</p>
+              </div>
+              <div class="col">
+                <a class="button fa fa-external-link" href="#">Visit</a>
+                <a class="button fa fa-youtube-play" href="#">Youtube</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <footer>
+    <p>&copy; Copyright 2023 <b>Adi Satrio Wibowo.</b> All Rights Reserved</p>
+  </footer>
+
+
+  <script>
+    AOS.init({
+      duration: 2500
+    });
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/jump.js/dist/jump.min.js"></script>
+
+  <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
